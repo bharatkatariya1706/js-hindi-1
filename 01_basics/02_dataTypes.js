0bacdfa61e6f5a45cd9c6946ce7255714495d697
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2024-05-09 16:22:10 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version  
+// alert(3+3) // we are using node.js , not browser
+//code readability should be high
+
+let name ="Bharat"
+let age =18
+let isLoggesIn = false
+
+// number=> 1,2,3. ....
+// bigInt
+//string=> " "
+//boolean=> true/false
+//null=> standalone value
+//undefined=> value not assigned
+//symbol=> uniqueness
+
+
+//object
+
+console.log(typeof("bharat"));
+console.log(typeof undefined);
+console.log(typeof null);
