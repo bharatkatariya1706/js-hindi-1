c939b5f626a524357ed967e1f3f72c05a802ac45
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2024-05-09 16:21:43 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =144553
+let accountEmail = "bharat@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId =2 //not allowed
+
+accountEmail = "raj@gmail.com"
+accountPassword="121313"
+accountCity="Indore"
+console.log(accountId);
+console.table([accountId , accountEmail, accountPassword , accountCity])
