c6b5a5f7fd3877eea32cf372fca36c31887de501
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2024-05-10 15:22:35 +0530

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score ="33"
+  
+// console.log( typeof score);
+
+
+let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false =>0
+
+
+//********** OPERATIONS *************//
+let value =3
+let negValue =-value
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 =" bharat"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+  //++++++++++++++++++++++++++++++++++++
+
+  // Stack(Primitive) , Heap(Non-primitive)
+      let myName = "BharatKatariya "
+      let newName = myName
+      newName="RajKatariya"
+     
+       console.log(myName); 
+       console.log(newName);
