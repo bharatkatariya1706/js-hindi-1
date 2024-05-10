c55acc26115845d13f49882e21513804744931e6
bharatkatariya1706 <bharatrajkatariya24@gmail.com>
2024-05-10 15:22:50 +0530

    stringsBasics

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const name = "Bharat";
+const repoCount = 50;
+// console.log(name+repoCount+" value");
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+//another way to declare a string
+const gameName = new String('bharat');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('r'));
